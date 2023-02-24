86f73ea51b3eb0ea70759221feb1431a8cc5d2b6
Peloso31 <115500108+Peloso31@users.noreply.github.com>
2023-02-24 08:14:54 -0300

    Cronometro

--- script.js
+++ script.js
@@ -1,92 +1,37 @@
-// Captura os elementos HTML
-const hoursElem = document.getElementById('hours');
-const minutesElem = document.getElementById('minutes');
-const secondsElem = document.getElementById('seconds');
-const millisecondsElem = document.getElementById('milliseconds');
-const iniciarBtn = document.getElementById('iniciar');
-const pararBtn = document.getElementById('parar');
-const voltaBtn = document.getElementById('volta');
-const voltaTBody = document.getElementById('volta-tbody');
+let cronometro;
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
 
-// Define o objeto timer com as propriedades necessárias
-const timer = {
-  startTime: 0,
-  elapsedTime: 0,
-  interval: null,
-  isRunning: false,
-  laps: []
-};
+function iniciarCronometro() {
+  cronometro = setInterval(() => {
+    const horarioBrasilia = moment().tz('America/Sao_Paulo');
+    segundos++;
 
-// Função para formatar o tempo
-function formatTime(time) {
-  let hours = Math.floor(time / 3600000);
-  let minutes = Math.floor((time % 3600000) / 60000);
-  let seconds = Math.floor((time % 60000) / 1000);
-  let milliseconds = time % 1000;
+    if (segundos === 60) {
+      segundos = 0;
+      minutos++;
 
-  return {
-    hours: hours.toString().padStart(2, '0'),
-    minutes: minutes.toString().padStart(2, '0'),
-    seconds: seconds.toString().padStart(2, '0'),
-    milliseconds: milliseconds.toString().padStart(3, '0')
-  };
+      if (minutos === 60) {
+        minutos = 0;
+        horas++;
+      }
+    }
+
+    const tempo = `${horarioBrasilia.format('HH:mm:ss')}`;
+    document.getElementById('tempo').textContent = tempo;
+  }, 1000);
 }
 
-// Função para atualizar o cronômetro
-function updateTimer() {
-  const currentTime = Date.now();
-  timer.elapsedTime = currentTime - timer.startTime;
-
-  const { hours, minutes, seconds, milliseconds } = formatTime(timer.elapsedTime);
-
-  hoursElem.textContent = hours;
-  minutesElem.textContent = minutes;
-  secondsElem.textContent = seconds;
-  millisecondsElem.textContent = milliseconds;
+function pararCronometro() {
+  clearInterval(cronometro);
 }
 
-// Função para iniciar o cronômetro
-function startTimer() {
-  timer.startTime = Date.now() - timer.elapsedTime;
-  timer.interval = setInterval(updateTimer, 10);
-  timer.isRunning = true;
+function resetarCronometro() {
+  clearInterval(cronometro);
+  segundos = 0;
+  minutos = 0;
+  horas = 0;
+  const horarioBrasilia = moment().tz('America/Sao_Paulo');
+  document.getElementById('tempo').textContent = horarioBrasilia.format('HH:mm:ss');
 }
-
-// Função para parar o cronômetro
-function stopTimer() {
-  clearInterval(timer.interval);
-  timer.interval = null;
-  timer.isRunning = false;
-}
-
-// Função para adicionar uma volta
-function addLap() {
-  if (timer.isRunning) {
-    const { hours, minutes, seconds, milliseconds } = formatTime(timer.elapsedTime);
-
-    timer.laps.push({ time: `${hours}:${minutes}:${seconds}.${milliseconds}` });
-
-    const newRow = voltaTBody.insertRow();
-    const cell1 = newRow.insertCell();
-    const cell2 = newRow.insertCell();
-    cell1.textContent = `Volta ${timer.laps.length}`;
-    cell2.textContent = timer.laps[timer.laps.length - 1].time;
-  }
-}
-
-// Adiciona os event listeners aos botões
-iniciarBtn.addEventListener('click', function() {
-  if (!timer.isRunning) {
-    startTimer();
-  }
-});
-
-pararBtn.addEventListener('click', function() {
-  if (timer.isRunning) {
-    stopTimer();
-  }
-});
-
-voltaBtn.addEventListener('click', function() {
-  addLap();
-});
